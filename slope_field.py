a4064ca5c56884a7122d1dc7a989cac3148a30fd
Nathaniel Bechard <natebechard@gmail.com>
2023-02-18 12:15:01 -0500

    update after 4 weeks

--- slope_field.py
+++ slope_field.py
@@ -3,19 +3,19 @@ from matplotlib import pyplot as plt
 import math
 
 
-def slope(t, y):
-    return  y*np.exp(-t/3) - 2*y**2
 #def slope(t, y):
-#    return 4 - 6*t - 2*y
+#    return  1 + (t * y)/2 + 0.5 * np.sin(2*t)
+def slope(t, y):
+    return (-t*y**2 -np.cos(t))/(np.exp(2*y)+y*t**2)
 
 def linear(t1, y1, slope, domain):
     y_val = y1 + (slope)*(domain-t1)
     return y_val
-T = np.linspace(-6, 4, 30)
-Y = np.linspace(-2, 3, 30)
+T = np.linspace(-4, 4, 30)
+Y = np.linspace(-4, 4, 30)
 for j in T:
     for k in Y:
-        length = 0.15
+        length = 0.2
         local_slope = slope(j, k)
         dt = math.sqrt((length**2)/((local_slope**2) + 1))
         dy = math.sqrt((length**2)-(dt**2))
@@ -28,7 +28,7 @@ for j in T:
         plt.arrow((j-(dt/2)), (k-(dy/2)), dt, dy, color='red',length_includes_head=True, head_length=.02, head_width=.03)
 
 plt.title("Directional Field")
-plt.plot(-7, -3)
+plt.plot(0, 0)
 plt.plot(5, 4)
 plt.grid(True)
 plt.show()
